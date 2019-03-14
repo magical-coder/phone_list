19f378d6fac4668153d0a5125313c4a69866c5a8
SAUBHAGYA SAHOO <saubhagyasahoo2@gmail.com>
2019-03-14 18:00:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      persons: [
+        {name: "sahoo", number: "8698657483"},
+        {name: 'jolly', number: '7261999059'}
+      ]
+    }
+  }
+
+  add = (obj) => {
+    let st = this.state;
+    st.persons.push(obj);
+    this.setState(st);
+  }
+
+  delete = (i) => {
+    let st = this.state;
+    let p = st.persons.filter((data, index) => index !== i);
+    st.persons = p;
+    this.setState(st);
+  }
+
   render() {
     return (
+      <body style="background-color:blue;">
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+		      Phone Register
         </header>
+
+        <ul>
+          {this.state.persons.map((data, index) => (<li key={index}>{data.name} {data.number} <button onClick={() => this.delete(index)}>delete</button></li>))}
+        </ul>
+        <button className="but" onClick={() => this.add({name: 'sanjay', number:'8698635501'})}>add</button>
       </div>
+      </body>
     );
   }
 }
